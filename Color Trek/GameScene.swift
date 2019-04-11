1e680c309a122c0406eed8f0e05b9f6691a5b0a6
Kamal Maged <Kamal_Maged@yahoo.com>
2019-04-11 21:53:44 +0200

    section 4 player motion

--- Color Trek/GameScene.swift
+++ Color Trek/GameScene.swift
@@ -10,78 +10,91 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var trackArray: [SKSpriteNode]? = [SKSpriteNode]()
+    var player: SKSpriteNode? = SKSpriteNode(imageNamed: "player")
+    var currentTrack = 0
+    var movetoTrack = false
+    let moveSound = SKAction.playSoundFileNamed("move.wav", waitForCompletion: false)
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    func setUpTracks() {
+        for i in 0...7 {
+            if let track = self.childNode(withName: "\(i)") {
+                trackArray?.append(track as! SKSpriteNode)
+            }
+        }
+        
+    }
+    func createPlayer() {
+        guard let playerposition = trackArray?.first?.position.x else { return }
+        player?.position = CGPoint(x: playerposition, y: self.size.height / 2)
+        self.addChild(player!)
+    }
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        setUpTracks()
+        createPlayer()
+        trackArray?.first?.color = UIColor.green
+      
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+    func moveToNextTrack() {
+        player?.removeAllActions()
+        movetoTrack = true
+        
+        guard let nextTrack = trackArray?[currentTrack + 1].position else {return}
+        if let player = self.player {
+           let moveAction = SKAction.move(to: CGPoint(x: nextTrack.x, y: player.position.y), duration: 0.2)
+            player.run(moveAction, completion: {
+                self.movetoTrack = false
+                })
+            currentTrack += 1
+            self.run(moveSound)
         }
+        
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+    func moveVertically(up: Bool) {
+        if up {
+            let moveAction = SKAction.moveBy(x: 0, y: 3, duration: 0.01)
+            let repeatAction = SKAction.repeatForever(moveAction)
+            player?.run(repeatAction)
+        } else {
+            let moveAction = SKAction.moveBy(x: 0, y: -3, duration: 0.01)
+            let repeatAction = SKAction.repeatForever(moveAction)
+            player?.run(repeatAction)
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        if let touch = touches.first {
+            let location = touch.previousLocation(in: self)
+            let node = self.nodes(at: location).first
+            if node?.name == "up" || node?.name == "upImg" {
+                moveVertically(up: true)
+            }
+            else if node?.name == "down" || node?.name == "downImg" {
+                moveVertically(up: false)
+            }
+            else if node?.name == "right" || node?.name == "rightImg" {
+                moveToNextTrack()
+            }
+            
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+       
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        if !movetoTrack {
+            player?.removeAllActions()
+        }
+      //  player?.removeAllActions()
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        if !movetoTrack {
+            player?.removeAllActions()
+        }
     }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
